d6c47bc53c57215cfeb2a1adc4661ce946e5d7ae
Jordano Tegner <jordano.tegner@gmail.com>
2020-09-20 12:11:50 -0300

    Adicionando rotas no React native

---
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const CreateRecord = () => {
+    return (
+        <Text>Hello create-record!</Text>
+    );
+}
+
+export default CreateRecord;
